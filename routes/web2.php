1607c58ef340f7533cc6314d0ad6b4d792782faf
Ramadhianto03 <mdwiramadhianto@gmail.com>
2023-04-15 13:44:20 +0700

    Belajar model controller

--- routes/web2.php
+++ routes/web2.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\FoodController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ('Hello World');
-    
-});
+
+// Route::get('/', function () {
+//     return ('Hello World');
+
+// });
+//tanpa controller
+// Route::get('/', function () {
+//     return view ('food'); 
+// });
+
+//dengan controller
+Route::get('/', [FoodController::class, 'food'] );
+
